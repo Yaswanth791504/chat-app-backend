12e9d3ea2695cecb7ead57eb966d7c7eb48c3f25
Yaswanth791504 <mallisettiyaswath@gmail.com>
2024-03-05 12:18:13 +0530

    urlfixed

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = require("./index.js");
 app.listen(process.env.PORT || 7846, () => {
   console.log(`App is listening on ${process.env.PORT}`);
   mongoose
-    .connect(process.env.URl.replace("<password>", process.env.PASSWORD))
+    .connect(process.env.MONGO_URL.replace("<password>", process.env.PASSWORD))
     .then(() => {
       console.log("Db is connected");
     })
